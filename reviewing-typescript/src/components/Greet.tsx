6e7b40445603d3821c6be18b38b826758f9d16a5
Mark_Leo <ratrateroo@gmail.com>
2022-07-26 12:05:58 +0800

    tried out basic types

---
+++ reviewing-typescript/src/components/Greet.tsx
@@ -0,0 +1,19 @@
+type Greet = {
+  name: string;
+  messageCount: number;
+  isLoggedIn: boolean;
+};
+
+export const Greet = (props: Greet) => {
+  return (
+    <div>
+      {props.isLoggedIn ? (
+        <h2>
+          Hello {props.name}. You have {props.messageCount}
+        </h2>
+      ) : (
+        <h2>Hello, welcome guest.</h2>
+      )}
+    </div>
+  );
+};
---
+++ reviewing-typescript/src/components/PersonList.tsx
@@ -0,0 +1,20 @@
+type PersonsListProps = {
+  names: {
+    first: string;
+    last: string;
+  }[];
+};
+
+export const PersonList = (props: PersonsListProps) => {
+  return (
+    <div>
+      {props.names.map((name, index) => {
+        return (
+          <h2 key={name.first + index}>
+            {name.first} {name.last}
+          </h2>
+        );
+      })}
+    </div>
+  );
+};
