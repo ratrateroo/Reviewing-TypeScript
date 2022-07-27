4848bab55f4f2c2da9f75bfb70b89a525362f2e3
Mark_Leo <ratrateroo@gmail.com>
2022-07-27 23:45:36 +0800

    added advance type for status component

--- reviewing-typescript/src/App.tsx
+++ reviewing-typescript/src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import { Greet } from "./components/Greet";
 import { Person } from "./components/Person";
 import { PersonList } from "./components/PersonList";
+import { Status } from "./components/Status";
 
 function App() {
   const personName = {
@@ -31,6 +32,7 @@ function App() {
       <Greet name="Mark" messageCount={100} isLoggedIn={false} />
       <Person name={personName} />
       <PersonList names={nameList} />
+      <Status status="loading" />
     </div>
   );
 }
