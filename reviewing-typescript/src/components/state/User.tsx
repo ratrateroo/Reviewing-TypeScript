e924b7baf02e2eeefda3b31a5de713f4cd49c90c
Mark_Leo <ratrateroo@gmail.com>
2022-08-17 16:54:45 +0800

    added type assertion example

--- reviewing-typescript/src/components/state/User.tsx
+++ reviewing-typescript/src/components/state/User.tsx
@@ -2,6 +2,7 @@
 //state can be AuthUser or Null
 //optional chaining is required to access the values of state objects
 
+//using type assertion for user status if the value of state is certain and will not be different shape
 import { useState } from "react";
 
 type AuthUser = {
@@ -9,8 +10,13 @@ type AuthUser = {
   email: string;
 };
 
+type UserStatus = {
+  status: string;
+};
+
 export const User = () => {
   const [user, setUser] = useState<AuthUser | null>(null);
+  const [userStatus, setUserStatus] = useState<UserStatus>({} as UserStatus);
   const handleLogin = () => {
     setUser({
       name: "Coleen Wing",
@@ -22,6 +28,7 @@ export const User = () => {
   };
   return (
     <div>
+      <h1>User status: {userStatus.status === "guest" ? "Guest" : "User"} </h1>
       <button onClick={handleLogin}>Login</button>
       <button onClick={handleLogout}>Logout</button>
       <div>User name is {user?.name}</div>
