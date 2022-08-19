431cb7cef007a752ad1c4915e2c5b63cff80e533
Mark_Leo <ratrateroo@gmail.com>
2022-08-19 21:01:00 +0800

    added extracting component

---
+++ reviewing-typescript/src/components/html/CustomComponent.tsx
@@ -0,0 +1,8 @@
+//using types from other component by extracting
+
+import React from "react";
+import { Greet } from "./Greet";
+
+export const CustomComponent = (props: React.ComponentProps<typeof Greet>) => {
+  return <div>{props.name}</div>;
+};
