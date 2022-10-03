8c3c7024fa9d39489bda8e9d68b22a4700b69e17
Mark_Leo <ratrateroo@gmail.com>
2022-10-03 17:20:29 +0800

    added sample use ref hook typing

---
+++ reviewing-typescript/src/components/hooks/useRefHook.tsx
@@ -0,0 +1,18 @@
+function Foo() {
+  // - If possible, prefer as specific as possible. For example, HTMLDivElement
+  //   is better than HTMLElement and way better than Element.
+  // - Technical-wise, this returns RefObject<HTMLDivElement>
+  const divRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // Note that ref.current may be null. This is expected, because you may
+    // conditionally render the ref-ed element, or you may forgot to assign it
+    if (!divRef.current) throw Error("divRef is not assigned");
+
+    // Now divRef.current is sure to be HTMLDivElement
+    doSomethingWith(divRef.current);
+  });
+
+  // Give the ref to an element so React can manage it for you
+  return <div ref={divRef}>etc</div>;
+}
