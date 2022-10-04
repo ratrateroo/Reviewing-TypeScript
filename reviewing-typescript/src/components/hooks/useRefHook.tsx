5d2181ad5b6799c4d182e45166a4afd2539e5b24
Mark_Leo <ratrateroo@gmail.com>
2022-10-04 18:57:53 +0800

    updated useRef  example

--- reviewing-typescript/src/components/hooks/useRefHook.tsx
+++ reviewing-typescript/src/components/hooks/useRefHook.tsx
@@ -1,10 +1,11 @@
+import React from "react";
 function Foo() {
   // - If possible, prefer as specific as possible. For example, HTMLDivElement
   //   is better than HTMLElement and way better than Element.
   // - Technical-wise, this returns RefObject<HTMLDivElement>
-  const divRef = useRef<HTMLDivElement>(null);
+  const divRef = React.useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
+  React.useEffect(() => {
     // Note that ref.current may be null. This is expected, because you may
     // conditionally render the ref-ed element, or you may forgot to assign it
     if (!divRef.current) throw Error("divRef is not assigned");
@@ -15,4 +16,18 @@ function Foo() {
 
   // Give the ref to an element so React can manage it for you
   return <div ref={divRef}>etc</div>;
+}
+
+function Bar() {
+  // Technical-wise, this returns MutableRefObject<number | null>
+  const intervalRef = React.useRef<number | null>(null);
+
+  // You manage the ref yourself (that's why it's called MutableRefObject!)
+  React.useEffect(() => {
+    intervalRef.current = setInterval(...);
+    return () => clearInterval(intervalRef.current);
+  }, []);
+
+  // The ref is not passed to any element's "ref" prop
+  return <button onClick={/* clearInterval the ref */}>Cancel timer</button>;
 }
